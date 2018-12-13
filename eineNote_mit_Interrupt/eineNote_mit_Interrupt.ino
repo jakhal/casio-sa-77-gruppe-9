c8fad239c4b124720e3ff036391d664a2f1e3e7c
jakhal <45066215+jakhal@users.noreply.github.com>
2018-12-13 12:08:58 +0100

    Add files via upload

---
+++ eineNote_mit_Interrupt/eineNote_mit_Interrupt.ino
@@ -0,0 +1,25 @@
+const int InterruptPin = 2;
+void setup() {
+  // put your setup code here, to run once:
+  DDRA=B11111111 ;
+  DDRC =B00000000;
+  PORTA=B11111111;
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+attachInterrupt(digitalPinToInterrupt(InterruptPin), playnote, LOW);
+PORTA =B11111111;
+}
+
+void playnote() {
+ /* start=millis();
+  unterschied=0;
+ while(unterschied<4){
+ */
+ PORTA = B01111111;
+ //unterschied=millis()-start;  
+ 
+}
+
+
+
