0344cb34ee34326459a7b000af9557e34ca7b913
jakhal <45066215+jakhal@users.noreply.github.com>
2018-12-19 15:36:11 +0100

    Add files via upload

---
+++ Tonausgabe_Gruppe9/Lied.ino
@@ -0,0 +1,18 @@
+void lied() {
+  g(takt*3/8*3/4);
+  a(takt/8*3/4);
+  g(takt/4*3/4);
+  e(takt*3/4*3/4);
+  g(takt*3/8*3/4);
+  a(takt/8*3/4);
+  g(takt/4*3/4);
+  e(takt*3/4*3/4);
+  dS(takt*4/8*3/4-resetT);
+  set1(resetT);
+  dS(takt/4*3/4);
+  h(takt*4/8);
+  cS(takt*4/8*3/4-resetT);
+  set1(resetT);
+  cS(takt*2/8*3/4);
+  g(takt*3/4);
+}
